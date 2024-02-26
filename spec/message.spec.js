33fe0c533a28c661142a6fd919ccb25d0c258281
r-vaughn <rebecca.a.vaughn@outlook.com>
2024-02-26 10:40:05 -0600

    Message testing complete and passing

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -6,4 +6,18 @@ const Command = require('../command.js');
 
 describe("Message class", function() {
 
+    it("throws error if a name is not passed into the construtor as the first parameter", function() {
+        expect(function() {new Message();}).toThrow(new Error("Name required."))
+    });
+
+    test("constructor sets name", function() { 
+        let output = new Message("Carl", ["run", "sit", "jump"]);
+        expect(output.name).toEqual("Carl");
+    });
+
+    test("contains a commands array passed into the constructor as the 2nd argument", function() {
+        let output = new Message("Carl", ["run", "sit", "jump"]);
+        expect(output.commands).toEqual(["run", "sit", "jump"]);
+    })
+
 });
