9f8d5b3e5e90a506f0c5e73f2dcbee57e433b920
r-vaughn <rebecca.a.vaughn@outlook.com>
2024-02-26 00:44:49 -0600

    Command class constructor testing passes

--- command.js
+++ command.js
@@ -1,10 +1,10 @@
 class Command {
    constructor(commandType, value) {
-     this.commandType = commandType;
-     if (!commandType) {
-       throw Error("Command type required.");
-     }
-     this.value = value;
+      this.commandType = commandType;
+        if (!commandType) {
+        throw Error("Command type required.");
+        }
+      this.value = value;
    }
  
  }
