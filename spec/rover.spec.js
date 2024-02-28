561a2c7185d78ae68cb1f41992d4c56415594376
r-vaughn <rebecca.a.vaughn@outlook.com>
2024-02-27 23:47:19 -0600

    Rover object TDD tests 10-11 passing

--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -10,40 +10,66 @@ describe("Rover class", function() {
 
   // 7 tests here!
   {
-    let commandsArray = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
-    let newMessage = new Message('Carl', commandsArray);
-    let rover = new Rover(98382);
-    let roverResponse = rover.receiveMessage(newMessage);
-  
-    test("constructor sets position and default values for mode and generatorWatts", function() {
+    {
+      let commandsArray = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+      let rover = new Rover(98382);
+      
+      test("constructor sets position and default values for mode and generatorWatts", function() {
         expect(rover.position).toEqual(98382);
         expect(rover.mode).toEqual('NORMAL');
         expect(rover.generatorWatts).toEqual(110);
-    });
+      });
+    }
+
+    { 
+      let commandsArray = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+      let newMessage = new Message('Carl', commandsArray);
+      let rover = new Rover(98382);
+      let roverResponse = rover.receiveMessage(newMessage);
+
+      test("response returned by receiveMessage contains the name of the message", function() {
+          //roverResponse is an object {message: Message.name, results:[ {command1}, {command2}] }, 
+          expect(roverResponse.message).toEqual(newMessage.name);
+      });
     
-    test("response returned by receiveMessage contains the name of the message", function() {
-        //roverResponse is an object {message: Message.name, results:[ {command1}, {command2}] }, 
-        expect(roverResponse.message).toEqual(newMessage.name);
-    });
-    
-     test("response returned by receiveMessage includes two results if two commands are sent in the message", function() {
-        //receiveMessage returns an object with 2 results if 2 commands are sent {message: Message.name, results: [ {command1}, {command2} ]}
-        expect(roverResponse.results).toHaveLength(2);
+      test("response returned by receiveMessage includes two results if two commands are sent in the message", function() {
+          //receiveMessage returns an object with 2 results if 2 commands are sent {message: Message.name, results: [ {command1}, {command2} ]}
+          expect(roverResponse.results).toHaveLength(2);
       });
 
-/*10test("responds correctly to the status check command", function() {
+    
+      test("responds correctly to the 'STATUS_CHECK' command", function() {
         //if commandsArray [ {command1}, {command2} ] includes 'STATUS_CHECK', receiveMessage returns an object { mode: X, generatorWatts: 110, position: 98382} at commandArray position for 'STATUS_CHECK'
-        expect(commandsArray).toContain('STATUS_CHECK');
-        expect(roverResponse.results).toHaveProperty(['roverStatus', rover]);          
+          let statusCheckCounts = 0;
+          for ( let i = 0; i < commandsArray.length; i++ ) {
+            const command = commandsArray[i];
+            if (command.commandType === 'STATUS_CHECK') {
+              statusCheckCounts += 1;
+              expect(roverResponse.results[i]).toHaveProperty( 'roverStatus', rover );
+              // expect(roverResponse.results[i]).toHaveProperty( 'roverStatus', { position: 123, mode: 'NORMAL', generatorWatts: 110 } );
+            }
+          }
+          expect(statusCheckCounts).toBeGreaterThan(0);
       });
-      
-/*11test("responds correctly to the mode change command", function () { 
-        //if commandsArray includes 'MODE_CHANGE' receiveMessage returns an object containing 'MODE_CHANGE' value at {mode: X}
-        expect(commandsArray).toContain('MODE_CHANGE'); 
-        expect(roverResponse.results).toHaveProperty('mode', 'LOW_POWER');
+    
+      // Test 11
+      test("responds correctly to the mode change command", function () { 
+          //if commandsArray includes 'MODE_CHANGE' receiveMessage returns an object containing 'MODE_CHANGE' value at {mode: X}
+          let modeChageCounts = 0;
+          for ( let i = 0; i < commandsArray.length; i++ ) {
+            const command = commandsArray[i];
+            if (command.commandType === 'MODE_CHANGE') {
+              modeChageCounts += 1;
+              expect(roverResponse.results[i]).toHaveProperty( 'completed', true );
+              // expect(roverResponse.results[i]).toHaveProperty( 'roverStatus', { position: 123, mode: 'NORMAL', generatorWatts: 110 } );
+              expect(rover.mode).toEqual(command.value);
+            }
+          }
+          expect(modeChageCounts).toBeGreaterThan(0);
       }); 
 
-/*12test("responds with false completed value when attempting to move in LOW_POWER mode", function() {
+//12
+      test("responds with false completed value when attempting to move in LOW_POWER mode", function() {
         //if rover includes {mode: LOW_POWER} will return {completed: false} and position remains the same
         expect(rover.mode).toContain('LOW_POWER');
         expect(rover.mode).toContain('MOVE', 0);
@@ -51,10 +77,12 @@ describe("Rover class", function() {
         expect(roverResponse.results).toHaveProperty('position', 98382);
       });
 
-/*13test("responds with the position for the move command", function() {
+/*13
+      test("responds with the position for the move command", function() {
         //MOVE command will update the rover's position with the position value in the command
         expect(rover.mode).toContain('MOVE', 0); 
         expect(roverResponse.results).toEqual(0)
       });*/
+    }
   }
 });
